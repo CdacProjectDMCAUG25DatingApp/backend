56eb2287c260f37377b7f947dce093825fe211d7
Chaitanya <chaitanyasundli165@gmail.com>
2025-12-07 20:33:51 +0530

    Endpoints related Userpreferences created in getlookup and user

--- routes/LookUpTables/getlookups.js
+++ routes/LookUpTables/getlookups.js
@@ -45,5 +45,113 @@ router.get("/job-Industry", (req, res) => {
     );
 });
 
+router.get("/lookingfor", (req, res) => {
+    pool.query(
+        "SELECT looking_for_id, name FROM lookingfor WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/opento", (req, res) => {
+    pool.query(
+        "SELECT open_to_id, name FROM opento WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/zodiac", (req, res) => {
+    pool.query(
+        "SELECT zodiac_id, name FROM zodiac WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/familyplan", (req, res) => {
+    pool.query(
+        "SELECT family_plan_id, name FROM familyplans WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/communicationstyle", (req, res) => {
+    pool.query(
+        "SELECT communication_style_id, name FROM communicationstyle WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/lovestyle", (req, res) => {
+    pool.query(
+        "SELECT love_style_id, name FROM lovestyle WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/drinking", (req, res) => {
+    pool.query(
+        "SELECT drinking_id, name FROM drinking WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/smoking", (req, res) => {
+    pool.query(
+        "SELECT smoking_id, name FROM smoking WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/workout", (req, res) => {
+    pool.query(
+        "SELECT workout_id, name FROM workout WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+router.get("/dietary", (req, res) => {
+    pool.query(
+        "SELECT dietary_id, name FROM dietary WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
+router.get("/sleepingHabit", (req, res) => {
+    pool.query(
+        "SELECT sleeping_habit_id, name FROM sleepingHabit WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+router.get("/religion", (req, res) => {
+    pool.query(
+        "SELECT religion_id, name FROM religion WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+router.get("/personalitytype", (req, res) => {
+    pool.query(
+        "SELECT personality_type_id, name FROM personalitytype WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+router.get("/pet", (req, res) => {
+    pool.query(
+        "SELECT pet_id, name FROM pet WHERE active = 1;",(err, data) => {
+            res.send(result.createResult(err,data));
+        }
+    );
+});
+
 
 module.exports =  router;
--- routes/user.js
+++ routes/user.js
@@ -45,7 +45,7 @@ router.post('/signin', (req, res) => {
 
 router.post('/signup', (req, res) => {
     const { name, email, password, mobile } = req.body
-    
+
     const sql = `INSERT INTO users(user_name,email,password,phone_number) VALUES (?,?,?,?)`
     // create the hashedpassword
     bcrypt.hash(password, config.SALT_ROUND, (err, hashedPassword) => {
@@ -59,24 +59,41 @@ router.post('/signup', (req, res) => {
 })
 
 router.post('/userprofile', (req, res) => {
-    const { gender,bio,religion,location,motherTongue,marital,dob,education,jobTitle,jobIndustry } = req.body
+    const { gender, bio, religion, location, motherTongue, marital, dob, education, jobTitle, jobIndustry } = req.body
     const uid = req.headers.uid
-    const sql = `INSERT INTO userprofile(uid,bio,gender,location,religion,mother_tongue,marital_status,dob,education,job_title,job_industry_id) VALUES (?,?,?,?,?,?,?,?,?,?,?)`     
-    pool.query(sql, [uid,bio,gender,location,religion,motherTongue,marital,dob,education,jobTitle,jobIndustry], (err, data) => {
+    const sql = `INSERT INTO userprofile(uid,bio,gender,location,religion,mother_tongue,marital_status,dob,education,job_title,job_industry_id) VALUES (?,?,?,?,?,?,?,?,?,?,?)`
+    pool.query(sql, [uid, bio, gender, location, religion, motherTongue, marital, dob, education, jobTitle, jobIndustry], (err, data) => {
         res.send(result.createResult(err, data))
-        })
+    })
 })
 
 router.get('/userprofile', (req, res) => {
     const uid = req.headers.uid
     const sql = `select * from userprofile where uid = ?`
-    console.log(req.body)       
-    pool.query(sql,[uid], (err, data) => {
+    console.log(req.body)
+    pool.query(sql, [uid], (err, data) => {
         res.send(result.createResult(err, data))
+    })
+})
+
+router.post('/userpreferences', (req, res) => {
+    const uid = req.headers.uid
+    const { lookingFor, openTo, zodiac, familyPlan, education, communicationStyle, lovestyle, drinking, smoking, workout
+        , dietary, sleepingHabit, Religion, personalityType, pet } = req.body
+    const sql = `insert into userpreferences(uid, looking_for_id, open_to_id, zodiac_id, family_plan_id, education_id, communication_style_id, love_style_id, drinking_id, smoking_id, workout_id, dietary_id, sleeping_habit_id, religion_id, personality_type_id, pet_id) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`
+    pool.query(sql, [uid, lookingFor, openTo, zodiac, familyPlan, education, communicationStyle, lovestyle, drinking, smoking, workout
+        , dietary, sleepingHabit, Religion, personalityType, pet], (err, data) => {
+            res.send(result.createResult(err, data))
         })
 })
 
-
-
+router.get('/userpreferences', (req, res) => {
+    const uid = req.headers.uid
+    const sql = `select * from userpreferences where uid = ?`
+    console.log(req.body)
+    pool.query(sql, [uid], (err, data) => {
+        res.send(result.createResult(err, data))
+    })
+})
 
 module.exports = router
